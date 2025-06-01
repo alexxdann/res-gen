4ab7be4378216f91c209858259984f447445c213
alexxdann <alexdandev@gmail.com>
2025-06-02 00:38:30 +0200

    refactor to confirm dialog custom scene

--- addons/res_gen/res_gen.gd
+++ addons/res_gen/res_gen.gd
@@ -1,72 +1,85 @@
 @tool
 extends EditorPlugin
-var plugin_file = preload("res://addons/res_gen/context_menu.gd")
-var context_menu: EditorContextMenuPlugin
-var editor_filesystem: EditorFileSystem
-var accept_dialog: AcceptDialog
-var input: LineEdit
-var vbox: VBoxContainer
-var component_name: String
-
-var path: String
+var plugin_file = preload("res://addons/res_gen/context_menu.gd");
+var dialog_scn = preload("res://addons/res_gen/ui/dialog/dialog.tscn");
+var context_menu: EditorContextMenuPlugin;
+var editor_filesystem: EditorFileSystem;
+var toast: EditorToaster;
+var dialog: Dialog;
+var component_name: String;
+var path: String;
 
 func _enter_tree() -> void:
 	editor_filesystem = EditorInterface.get_resource_filesystem();
+	toast = EditorInterface.get_editor_toaster();
+	dialog = dialog_scn.instantiate();
 	
-	input = LineEdit.new();
-	input.placeholder_text = "component name";
-	input.text_changed.connect(_change_component_name);
-	
-	vbox = VBoxContainer.new();
-	vbox.add_child(input);
-	
-	accept_dialog = AcceptDialog.new();
-	accept_dialog.title = "Create component";
-	accept_dialog.register_text_enter(input);
-	accept_dialog.add_cancel_button("Cancel");
-	accept_dialog.confirmed.connect(_accept_confirm);
-	accept_dialog.canceled.connect(_cancel_confirm);
-	accept_dialog.add_child(vbox);
-	add_child(accept_dialog);
+	dialog.component_name_changed.connect(_change_component_name);
+	dialog.confirmed.connect(_accept_confirm);
+	dialog.canceled.connect(_cancel_confirm);
+	add_child(dialog)
 	
 	context_menu = plugin_file.new();
-	context_menu.item_clicked.connect(_open_context_menu);
+	context_menu.item_clicked.connect(_click_create_component_item);
 	add_context_menu_plugin(EditorContextMenuPlugin.ContextMenuSlot.CONTEXT_SLOT_FILESYSTEM, context_menu);
-	  
-func _open_context_menu(p):
-	path = p
-	accept_dialog.popup_centered(Vector2(600, 100))
-	input.grab_focus()
+	
+func _click_create_component_item(p: String) -> void:
+	if DirAccess.open("res://").dir_exists(p):
+		open_dialog(p);
+	else:
+		toast.push_toast(
+			"Choose directory, not " + p.get_file(), 
+			EditorToaster.SEVERITY_WARNING, 
+			p.simplify_path()
+		);
 
 func _accept_confirm():
-	var base_src = path+component_name+"/"+component_name;
-	var script = GDScript.new();
-	var node_to_save = Node.new();
-	var dir=DirAccess.open(path);
-	var scene = PackedScene.new();
-	
-	dir.make_dir(component_name);
-	
-	script.source_code = "extends Node";
-	script.resource_name = component_name;
-	script.resource_path = base_src + ".gd";
-	ResourceSaver.save(script, script.resource_path);
-	
-	node_to_save.name = component_name.capitalize();
-	node_to_save.set_script(load(script.resource_path));
-	scene.resource_name = component_name;
-	scene.pack(node_to_save);
-	ResourceSaver.save(scene, base_src+".tscn");
+	if component_name.length() > 0:
+		var type = dialog.get_options_value();
+		var base_src = path.path_join(component_name).path_join(component_name);
+		var script = GDScript.new();
+		var node_to_save = ClassDB.instantiate(type);
+		var dir = DirAccess.open(path);
+		var scene = PackedScene.new();
+		
+		dir.make_dir(component_name);
+		
+		script.source_code = """extends {type}
 
-	editor_filesystem.scan();
-	input.text = "";
-	
+func _ready():
+	pass""".format({"type": type});
+		
+		script.resource_name = component_name;
+		script.resource_path = base_src + ".gd";
+		ResourceSaver.save(script, script.resource_path);
+		
+		node_to_save.name = component_name.capitalize();
+		node_to_save.set_script(load(script.resource_path));
+		scene.resource_name = component_name;
+		scene.pack(node_to_save);
+		ResourceSaver.save(scene, base_src+".tscn");
+
+		editor_filesystem.scan();
+		dialog.input.text = "";
+	else:
+		toast.push_toast(
+			"Type component name", 
+			EditorToaster.SEVERITY_WARNING
+		);
+
 func _cancel_confirm():
-	input.text = "";
-	
+	component_name = "";
+	dialog.input.text = "";
+	pass
+
 func _change_component_name(new_text: String):
 	component_name = new_text;
 	
+func open_dialog(p: String):
+	path = p;
+	dialog.popup_centered();
+	dialog.options.grab_focus();	
+	
 func _exit_tree() -> void:
-	accept_dialog.queue_free();
+	dialog.queue_free();
 	remove_context_menu_plugin(context_menu);
---
+++ addons/res_gen/ui/dialog/dialog.gd
@@ -0,0 +1,29 @@
+@tool
+extends ConfirmationDialog
+class_name Dialog
+
+@onready var input: LineEdit = $Inputs/Input;
+@onready var options: OptionButton = $Inputs/Options;
+
+var btn_size = Vector2(50, 25);
+
+signal component_name_changed(new_text: String);
+
+func _ready() -> void:
+	register_text_enter(input);
+	get_ok_button().size = btn_size;
+	get_cancel_button().size = btn_size;
+	
+	options.grab_focus();
+	options.get_selected_metadata()
+	for type in ClassDB.get_class_list():
+		if ClassDB.is_parent_class(type, "Node"):
+			options.add_item(type);
+
+
+func _on_input_text_changed(new_text: String) -> void:
+	component_name_changed.emit(new_text);
+	
+func get_options_value() -> String:
+	return options.get_item_text(options.get_selected_id());
+	
